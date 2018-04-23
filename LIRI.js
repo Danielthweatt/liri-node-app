4cdb13db2cb02de5ece20d435b2f7e1eb5ec86dd
Danielthweatt <dthweatt192@yahoo.com>
2018-04-22 23:27:32 -0500

    worked on js

--- LIRI.js
+++ LIRI.js
@@ -7,3 +7,18 @@ const keys = require("./keys.js");
 
 const spotify = keys.spotify;
 const client = keys.twitter;
+
+// User Command
+
+const userArg = process.argv[2];
+
+switch (userArg) {
+    case "my-tweets":
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        break;
+    case "do-what-it-says":
+        break;
+}

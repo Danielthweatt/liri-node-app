25783f653f7ed846754a685ebf77cc8a6d2cc228
Danielthweatt <dthweatt192@yahoo.com>
2018-04-23 22:23:25 -0500

    worked on js

--- LIRI.js
+++ LIRI.js
@@ -13,11 +13,11 @@ const client = new Twitter(keys.twitter);
 
 // User Command
 
-const userArg = process.argv[2];
+const userArg1 = process.argv[2];
 
 // Response to User Command
 
-switch (userArg) {
+switch (userArg1) {
     case 'my-tweets':
         client.get('statuses/home_timeline', function(error, tweets, response){
             if (error) {
@@ -31,32 +31,32 @@ switch (userArg) {
         });
         break;
     case 'spotify-this-song':
-        let queryString;
+        let spotifyQueryString;
         if (process.argv.length === 3) {
-            console.log('Default (because you did not enter a song to search Spotify for):')
+            console.log('Default (because you did not enter a song to search Spotify for):');
             console.log(' Song Name: The Sign');
             console.log('  Album: The Sign (US Album) [Remastered]');
             console.log('   Artist: Ace of Base');
             console.log('   Preview: https://p.scdn.co/mp3-preview/4c463359f67dd3546db7294d236dd0ae991882ff?cid=b6e68f2cb049416ab7b0e04c7f5eed53');
         } else if (process.argv.length === 4) {
-            queryString = process.argv[3];
+            spotifyQueryString = process.argv[3];
         } else {
-            queryString = process.argv[3];
+            spotifyQueryString = process.argv[3];
             for (let i = 4; i < process.argv.length; i++) {
-                queryString += ` ${process.argv[i]}`;
+                spotifyQueryString += ` ${process.argv[i]}`;
             }
         }
-        if (queryString) {
+        if (spotifyQueryString) {
             spotify.search({
                 type: 'track', 
-                query: queryString
+                query: spotifyQueryString
             }, function(error, data){
                 if (error) {
                     console.log(`Error: ${error}`);
                 }  else {
                     if (data.tracks.total > 0) {
                         console.log('Here are (up to) 20 of the top search results for your song from Spotify:');
-                        console.log(` Song Name: ${queryString}`);
+                        console.log(` Song Name: ${spotifyQueryString}`);
                         data.tracks.items.forEach(function(track){
                             console.log(`  Album: ${track.album.name}`);
                             track.album.artists.forEach(function(artist){
@@ -74,6 +74,42 @@ switch (userArg) {
         }
         break;
     case 'movie-this':
+        let omdbQueryString;
+        if (process.argv.length === 3) {
+            console.log('Default (because you did not enter a song to search Spotify for):');
+            console.log(`Title: `);
+            console.log(`Release Year: `);
+            console.log(`IMDB Rating: `);
+            console.log(`Rotten Tomatoes Rating: `);
+            console.log(`Country Produced In: `);
+            console.log(`Language: `);
+            console.log(`Plot: `);
+            console.log(`Actors: `);
+        } else if (process.argv.length === 4) {
+            omdbQueryString = process.argv[3];
+        } else {
+            omdbQueryString = process.argv[3];
+            for (let i = 4; i < process.argv.length; i++) {
+                omdbQueryString += ` ${process.argv[i]}`;
+            }
+        }
+        if (omdbQueryString) {
+            request(`https://www.omdbapi.com/?t=${omdbQueryString}&y=&plot=short&apikey=trilogy`, function(error, response, body){
+                if (error) {
+                    console.log(`Error: ${error}`);
+                } else {
+                    console.log(body);
+                    console.log(`Title: ${body.Title}`);
+                    console.log(`Release Year: ${body.Year}`);
+                    console.log(`IMDB Rating: `);
+                    console.log(`Rotten Tomatoes Rating: `);
+                    console.log(`Country Produced In: ${body.Country}`);
+                    console.log(`Language: ${body.Language}`);
+                    console.log(`Plot: ${body.Plot}`);
+                    console.log(`Actors: ${body.Actors}`);
+                }
+            });
+        }
         break;
     case 'do-what-it-says':
         break;
